fd05c67be664860b5725c9db2c1a452a811a12c6
Maria <marijka000@gmail.com>
2023-01-21 22:22:24 +0300

    New Card

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.List;
+
 public class CardTest {
     private WebDriver driver;
 
@@ -21,24 +23,39 @@ public class CardTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
     }
 
-    @AfterAll
+    @AfterEach
     void tearDown() {
-            driver.quit();
-        }
+        driver.quit();
+        driver = null;
+    }
 
     @Test
     void shouldSendRequest() {
         driver.get("http://localhost:9999");
-        WebElement form = driver.findElement(By.cssSelector("[class] form"));
-        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Коновалова Мария");
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79997776655");
-        form.findElement(By.cssSelector("[data-test-id=agreement")).click();
-        form.findElement(By.cssSelector("type=button")).click();
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Коновалова Мария");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79997776655");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
 
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("data-test-id=order-success")).getText().trim();
+        String actual = driver.findElement(By.tagName("p")).getText().trim();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSendRequest2() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Коновалова Мари-Эн");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79997776655");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.tagName("p")).getText().trim();
 
         Assertions.assertEquals(expected, actual);
     }
